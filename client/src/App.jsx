4975790f9a9597118fa0fb058cbeb114357ff491
Asad <129451320+Asadul09@users.noreply.github.com>
2026-03-12 16:29:45 +0600

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerURL = "https://vivabot-6ml6.onrender.com";
+export const ServerURL = "https://vivabot-6ml6.onrender.com/";
 
 function App() {
   const dispatch = useDispatch();
